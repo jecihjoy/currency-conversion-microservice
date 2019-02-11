6b8f76390b669c22301c562d4da1d98cf0432d94
jecihjoy <jecihjoy@gmail.com>
2019-02-11 17:01:56 +0300

    app setup

---
+++ src/main/java/org/github/jess/currencyconversion/currencyconversionmicroservice/CurrencyconversionMicroserviceApplication.java
@@ -0,0 +1,14 @@
+package org.github.jess.currencyconversion.currencyconversionmicroservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CurrencyconversionMicroserviceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CurrencyconversionMicroserviceApplication.class, args);
+	}
+
+}
+
---
+++ src/test/java/org/github/jess/currencyconversion/currencyconversionmicroservice/CurrencyconversionMicroserviceApplicationTests.java
@@ -0,0 +1,19 @@
+/*
+package org.github.jess.currencyconversion.currencyconversionmicroservice;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class CurrencyconversionMicroserviceApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
+
+*/
